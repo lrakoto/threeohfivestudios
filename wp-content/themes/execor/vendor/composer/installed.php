397a6202e75d2f0d70a4112d3b4271621a7655bc
Victoria Rajaonarivony <victoriarajaonarivony@Mac.lan>
2026-03-26 18:49:18 -0700

    Initial commit: execor theme and vamtam plugin from GoDaddy

---
+++ wp-content/themes/execor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2bfb5315bdb2a4d20567c9ab80aa23140728ee97',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2bfb5315bdb2a4d20567c9ab80aa23140728ee97',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
